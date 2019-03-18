bf076164c9de8705f4e3337a3a83d61c218f4c50
Martin Mercer <martinmercer1@gmail.com>
2019-03-17 21:05:39 -0600

    added rest of deck tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -18,4 +18,40 @@ class DeckTest < Minitest::Test
     assert_equal [], deck.cards
   end
 
+  def test_it_can_have_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal cards, deck.cards
+  end
+
+  def test_it_can_count_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
+  end
+
+  def test_it_can_show_cards_in_specific_categories
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+    assert_equal [card_1], deck.cards_in_category(:Geography)
+    assert_equal [], deck.cards_in_category("Pop Culture")
+  end
+
 end
